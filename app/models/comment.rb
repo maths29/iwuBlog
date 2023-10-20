effc9a76e4ca6ecc7f229ce452fc1eb4d310108a
mathsoft29 <iwuconcept@gmail.com>
2023-10-20 09:39:46 +0100

    Fix the user model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-  
-    after_save :update_commentcounter
-  
-    private
-  
-    def update_commentcounter
-      post.update(CommentsCounter: post.comments.count)
-    end
+  belongs_to :user
+  belongs_to :post
+
+  after_save :update_commentcounter
+
+  private
+
+  def update_commentcounter
+    post.update(CommentsCounter: post.comments.count)
   end
+end
--- app/models/likes.rb
+++ app/models/likes.rb
@@ -1,12 +1,12 @@
 class Like < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-  
-    after_save :updatepostcounter
-  
-    private
-  
-    def updatepostcounter
-      post.update(likescounter: post.likes.count)
-    end
+  belongs_to :user
+  belongs_to :post
+
+  after_save :updatepostcounter
+
+  private
+
+  def updatepostcounter
+    post.update(likescounter: post.likes.count)
   end
+end
