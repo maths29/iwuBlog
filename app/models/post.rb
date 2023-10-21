7638dc99012c81c331253fb81d5c872ff3a180eb
mathsoft29 <iwuconcept@gmail.com>
2023-10-21 16:25:48 +0100

    Add the model validations to the app

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,11 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  validates :Title, presence: true, length: { maximum: 250 }
+  validates :likes_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  after_save :update_counter
 
   def most_recent_comments
     comments.order(created_at: :desc).limit(5)
