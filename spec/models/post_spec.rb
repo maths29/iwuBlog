deacdf3ca483e9bfc386dcffb0c4c5c933603980
mathsoft29 <iwuconcept@gmail.com>
2023-10-21 17:46:35 +0100

    fix the linter errors

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,7 +14,9 @@ RSpec.describe Post, type: :model do
   end
   it 'should be invalid post [maximum length for title is 250]' do
     post.Title = '
-    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero,
+    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+     (The Extremes of Good and Evil) by Cicero,
+
     written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance.
     The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
     expect(post).to_not be_valid
