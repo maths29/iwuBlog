a4c76122b10012c8619f443b563f2ff167e28673
mathsoft29 <iwuconcept@gmail.com>
2023-11-03 22:10:21 +0100

    Fix stylint errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show] do
+    end
+  end
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
-   get '/home', to: 'app#home'
-  root 'app#home'
   # Defines the root path route ("/")
   # root "articles#index"
 end
